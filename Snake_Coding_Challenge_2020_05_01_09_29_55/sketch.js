01dd61bc376bdeddcc4d335098907242564ec2ab
boltknight21 <62507522+boltknight21@users.noreply.github.com>
2020-05-01 15:25:43 +0530

    Add files via upload

---
+++ Snake_Coding_Challenge_2020_05_01_09_29_55/sketch.js
@@ -0,0 +1,67 @@
+
+let snake;
+let rez = 20;
+let food;
+let w;
+let h;
+var Image;
+/*function preload(){
+ Image=loadImage("Snake_walk.png");
+
+}*/
+
+  function setup() {
+  createCanvas(400, 400);
+    
+  w = floor(width / rez);
+  h = floor(height / rez);
+  frameRate(5);
+  snake = new Snake();
+  foodLocation();
+}
+
+function foodLocation() {
+  let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
+
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    snake.setDir(1, 0);
+  } else if (keyCode === DOWN_ARROW) {
+    snake.setDir(0, 1);
+  } else if (keyCode === UP_ARROW) {
+    snake.setDir(0, -1);
+  } else if (key == ' ') {
+    snake.grow();
+  }
+
+}
+
+function draw() {
+  scale(rez);
+  background("lightblue");
+  if (snake.eat(food)) {
+    foodLocation();
+  }
+  snake.update();
+  snake.show();
+
+
+  if (snake.endGame()) {
+    print("GAME OVER")
+    background("white");
+  
+    noLoop();
+
+  
+  }
+
+  noStroke();
+  fill("orange");
+  rect(food.x, food.y, 1, 1);
+}
